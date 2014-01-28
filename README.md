7ab485f12a6030042dc0b7f83259272941a98c0d
Sam Dobson <sjd333@gmail.com>
2014-01-28 23:25:42 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,53 @@
+Quickstart contact form processing
+----------------------------------
+
+This will be useful if...
+
+* You want to forward a simple contact form to your email
+* You have a (S3) static site and don't want to run a server
+
+Usage
+-----
+
+Register your email:
+
+```bash
+    $ curl --data "email=<your_email>" https://fwdform.herokuapp.com/register
+    Token: 780a8c9b-dc2d-4258-83af-4deefe446dee
+    
+```
+
+Test (optional):
+
+```bash
+    $ curl --data "email=person@form.com&name=person&message=hello" \
+           https://fwdform.herokuapp.com/user/<token>
+```
+
+Put into action:
+
+```html
+<form action="https://fwdform.herokuapp.com/user/<token>">
+  Email: <input type="text" name="name"><br>
+  Name: <input type="text" name="email"><br>
+  Message: <textarea name="message" cols="40" rows="5"></textarea>
+  <input type="submit" value="Send Message">
+</form> 
+```
+
+NB: Required parameters are: `email`, `name` and `message`. Other parameters will be ignored.
+
+Privacy concerns?
+-----------------
+
+Spin up your own free [Heroku](http://www.heroku.com) instance. A [Mandrill](http://mandrill.com) account required for email delivery.
+
+```bash
+    $ git clone https://github.com/samdobson/fwdform.git
+    $ heroku create
+    $ heroku config:set MANDRILL_API_KEY=<KEY>
+    $ heroku addons:add heroku-postgresql:dev
+    $ heroku pg:promote HEROKU_POSTGRESQL_COLOR
+    $ heroku ps:scale web=1
+```
+
---
+++ app.py
@@ -0,0 +1,63 @@
+import os
+from uuid import uuid4
+
+from mandrill import Mandrill
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask import Flask, request, redirect, abort
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+mandrill_client = Mandrill(os.environ['MANDRILL_API_KEY'])
+db = SQLAlchemy(app)
+
+class User(db.Model):
+
+    def __init__(self, email):
+        self.email = email
+        self.uuid = str(uuid4())
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(200), unique=True)
+    uuid = db.Column(db.String(36), unique=True)
+
+@app.route('/')
+def index():
+    return redirect('http://samdobson.github.io/fwdform')
+
+@app.route('/register', methods=['POST'])
+def register():
+    user = User.query.filter_by(email=request.form['email']).first()
+    if user:
+        return ('Email already registered', 403)
+    user = User(request.form['email'])
+    db.session.add(user)
+    db.session.commit()
+    return "Token: {}".format(user.uuid)
+
+@app.route('/user/<uuid>', methods=['POST'])
+def forward(uuid):
+    user = User.query.filter_by(uuid=uuid).first()
+    if not user:
+        return ('User not found', 406)
+    message = {
+               'to': [{'email': user.email}],
+               'from_email': request.form['email'],
+               'subject': 'Message from {}'.format(request.form['name']),
+               'text': request.form['message'],
+              }
+    result = mandrill_client.messages.send(message=message)
+    if result[0]['status'] != 'sent':
+        abort(500)
+    return 'Your message was sent successfully'
+
+@app.errorhandler(400)
+def bad_parameters(e):
+    return ('<p>Missing information. Press the back button to complete '
+            'the empty fields.</p><p><i>Developers: we were expecting '
+            'the parameters "name", "email" and "message". You might '
+            'also consider using JS validation.</i>', 400)
+
+@app.errorhandler(500)
+def error(e):
+    return ('Sorry, something went wrong!', 500)
+
